5aa68f86c71400c3172e206c2cef2b8d9d968a47
root <admin@vpntech.net>
2020-01-17 20:31:21 -0500

    breka up client scripts

--- index.js
+++ index.js
@@ -129,6 +129,7 @@ proc.stdout.on('data', function(out) {
             J.CG = {
              'cmds': {
                'create': '/bin/cgcreate -a root:root -t root:root -g cpu,cpuacct,memory,pids,blkio:' + J.CG.name,
+               'classify': '/bin/cgclassify -g cpu,memory:' + J.CG.name + ' ' + String(pR.pid),
              },
             }
 
