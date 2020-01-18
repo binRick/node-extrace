a72d2d475d69968a9ae22dd164a1cb7cc7cd875f
root <admin@vpntech.net>
2020-01-17 20:01:40 -0500

    breka up client scripts

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ var handleInsert = function(ex, _cb) {
         delete ex.type;
         var query = connection.query('INSERT INTO execs SET ?', ex, function(error, results, fields) {
             _cb(error);
-            if(debug)
+            if (debug)
                 l(c.yellow('Inserted Row #') + c.black.bgWhite(results.insertId) + c.yellow('!'));
         });
 
@@ -44,7 +44,7 @@ var handleInsert = function(ex, _cb) {
         delete ex.type;
         var query = connection.query('UPDATE execs SET exit_code = ?, time = ?, ended_ts = NOW() where pid = ?', [ex.code, ex.time, ex.pid], function(error, results, fields) {
             _cb(error);
-            if(debug)
+            if (debug)
                 l(c.green('Updated Row #') + c.black.bgWhite(ex.pid) + c.green('!'));
         });
     } else {
@@ -82,80 +82,79 @@ var proc = child.spawn(bin, args, {
     shell: true
 });
 proc.stdout.on('data', function(out) {
-    out = out.toString();
-    startedProcesses = [];
-    endedProcesses = [];
-    var outLines = out.split("\n").filter(function(o) {
-        return o;
-    });
-    _.each(outLines, function(o) {
-        pR = {};
-        if (debug)
-            l('out>> ', o);
-        var spaceOut = o.split(' ');
-        if (spaceOut[0][spaceOut[0].length - 1] == '+') {
-            pR.type = 'start';
-            pR.user = spaceOut[1].replace('<', '').replace('>', '');
-            pR.cmd = spaceOut.slice(2, spaceOut.length).join(' ').trim();
-            var te = pR.cmd.split('   '),
-                pRenv1 = te[te.length - 1].split(' ');
-            pR.env = {};
-            _.each(pRenv1, function(pe) {
-                pej = pe.split('=');
-                pR.env[pej[0]] = pej[1];
+            out = out.toString();
+            startedProcesses = [];
+            endedProcesses = [];
+            var outLines = out.split("\n").filter(function(o) {
+                return o;
             });
-            pR.env = JSON.stringify(pR.env);
-            pR.cmd = te.slice(0, te.length - 1).join(' ');
-            pR.cwd = pR.cmd.split(' % ')[0];
-            pR.cmd = pR.cmd.replace(pR.cwd + ' % ', '');
-	    var buff = new Buffer(o);
-	    pR.line_b64 = buff.toString('base64');
-	    pR.line = buff;
+            _.each(outLines, function(o) {
+                    pR = {};
+                    if (debug)
+                        l('out>> ', o);
+                    var spaceOut = o.split(' ');
+                    if (spaceOut[0][spaceOut[0].length - 1] == '+') {
+                        pR.type = 'start';
+                        pR.user = spaceOut[1].replace('<', '').replace('>', '');
+                        pR.cmd = spaceOut.slice(2, spaceOut.length).join(' ').trim();
+                        var te = pR.cmd.split('   '),
+                            pRenv1 = te[te.length - 1].split(' ');
+                        pR.env = {};
+                        _.each(pRenv1, function(pe) {
+                            pej = pe.split('=');
+                            pR.env[pej[0]] = pej[1];
+                        });
+                        pR.env = JSON.stringify(pR.env);
+                        pR.cmd = te.slice(0, te.length - 1).join(' ');
+                        pR.cwd = pR.cmd.split(' % ')[0];
+                        pR.cmd = pR.cmd.replace(pR.cwd + ' % ', '');
+                        var buff = new Buffer(o);
+                        pR.line_b64 = buff.toString('base64');
+                        pR.line = buff;
 
-        var TE_CMD = te[0].split(' ');
+                        var TE_CMD = te[0].split(' ');
 
 
-        var J = {
-            'te_type': typeof(te),
-            '_PWD': te[0].split(' ')[0],
-            '_EXEC': te[0].split(' ')[2],
-            '_CMD': TE_CMD.splice(2, TE_CMD.length-2).join(' '),
-        }
-        J._ARGS = J._CMD.split(' ').splice(1,J._CMD.split(' ').length-2).join(' ');
+                        var J = {
+                            'te_type': typeof(te),
+                            '_PWD': te[0].split(' ')[0],
+                            '_EXEC': te[0].split(' ')[2],
+                            '_CMD': TE_CMD.splice(2, TE_CMD.length - 2).join(' '),
+                        }
+                        J._ARGS = J._CMD.split(' ').splice(1, J._CMD.split(' ').length - 2).join(' ');
 
-        pR._cmd = J._CMD;
-        pR._args = J._ARGS;
-        pR.exec = J._EXEC;
-	    pR.json = JSON.stringify(J);
-        } else if (spaceOut[0][spaceOut[0].length - 1] == '-') {
-            pR.type = 'end';
-            pR.exec = spaceOut[1];
-            pR.code = spaceOut[3].split('=')[1];
-            pR.time = spaceOut[4].split('=')[1].trim();
-        } else {
-            l('Unknown Output: ' + o);
-            process.exit(1)
-        }
-        pR.pid = o.slice(0, spaceOut[0].length - 1);
-        if (debug){
-            l(pj.render(pR) + "\n");
-        }
-if(pR.cmd=='' || pR.cmd == "''"){
-        if (debug){
-l('  ignoring null exec..');
+                        pR._cmd = J._CMD;
+                        pR._args = J._ARGS;
+                        pR.exec = J._EXEC;
+                        pR.json = JSON.stringify(J);
+                    } else if (spaceOut[0][spaceOut[0].length - 1] == '-') {
+                        pR.type = 'end';
+                        pR.exec = spaceOut[1];
+                        pR.code = spaceOut[3].split('=')[1];
+                        pR.time = spaceOut[4].split('=')[1].trim();
+                    } else {
+                        l('Unknown Output: ' + o);
+                        process.exit(1)
+                    }
+                    pR.pid = o.slice(0, spaceOut[0].length - 1);
+                    if (debug) {
+                        l(pj.render(pR) + "\n");
+                    }
+                    if (pR.cmd == '' || pR.cmd == "''") {
+                        if (debug) {
+                            l('  ignoring null exec..');
 
-}else{
-            handleInsert(pR, function(e) {
-                if (e) throw e;
-            });
-}
-    });
-});
-proc.stderr.on('data', function(err) {
-    err = err.toString();
-    l('err>> ', err);
-});
+                        } else {
+                            handleInsert(pR, function(e) {
+                                if (e) throw e;
+                            });
+                        }
+                    });
+            }); proc.stderr.on('data', function(err) {
+            err = err.toString();
+            l('err>> ', err);
+        });
 
-proc.on('exit', function(code) {
-    l(c.red('extrace exited with code ' + code));
-});
+        proc.on('exit', function(code) {
+            l(c.red('extrace exited with code ' + code));
+        });
