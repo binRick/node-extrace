dd66f4e8823f8972d9f1e864e78140e493c0278f
root <admin@vpntech.net>
2020-01-17 20:05:25 -0500

    breka up client scripts

--- summary.sh
+++ summary.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 SQL="select distinct exec from execs"
 cmd="mysql extrace -e \"$SQL\""
+echo $cmd
+eval $cmd
 
+
+SQL="SELECT exec, COUNT(*) as frequency\
+FROM execs \
+GROUP BY exec \
+ORDER BY COUNT(*) DESC \
+LIMIT 25; \
+"
+cmd="mysql extrace -e \"$SQL\""
 echo $cmd
 eval $cmd
