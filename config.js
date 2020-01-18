5fec1bbb0bc9a23a3f002e50d325909a04502fa4
root <admin@vpntech.net>
2020-01-18 01:04:25 -0500

    breka up client scripts

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 module.exports = {
+    cg_index: {
+     prefix: 'NODE_EXTRACE',
+     cmds: {
+      create: 'cgcreate -a root:root -t root:root -g cpu,cpuacct,memory,pids,blkio:NODE_EXTRACE',
+     },
+    },   
     cg_prefix: 'EXTRACE_',
     cg_execs: [
     ],
