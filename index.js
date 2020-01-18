d6060076ba6f52e1ff8dc68c0d703a90684c2305
root <admin@vpntech.net>
2020-01-17 22:16:28 -0500

    breka up client scripts

--- index.js
+++ index.js
@@ -59,8 +59,8 @@ var handleInsert = function(ex, _cb) {
         };
 
         ex.json_end.cg_paths = {};
-//        ex.json_end.cg_paths.cpuacct = '/sys/fs/cgroup/cpuacct/' + ex.json_end.cg + '/cpuacct.usage';
-//        ex.json_end.cg_paths.max_mem_bytes = '/sys/fs/cgroup/memory/' + ex.json_end.cg + '/memory.max_usage_in_bytes';
+        //        ex.json_end.cg_paths.cpuacct = '/sys/fs/cgroup/cpuacct/' + ex.json_end.cg + '/cpuacct.usage';
+        //        ex.json_end.cg_paths.max_mem_bytes = '/sys/fs/cgroup/memory/' + ex.json_end.cg + '/memory.max_usage_in_bytes';
 
         ex.json_end = JSON.stringify(ex.json_end);
 
