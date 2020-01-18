f11d785bcf831b8a1d4d3d226f3983123fb2c662
root <admin@vpntech.net>
2020-01-18 00:54:23 -0500

    breka up client scripts

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 env|grep __NODE
-if [[ "__NODE_EXTRACE_INSTALL" != "1" ]]; then
+if [[ "$__NODE_EXTRACE_INSTALL" != "1" ]]; then
     export __NODE_EXTRACE_INSTALL=1 
     exec ${BASH_SOURCE[0]} $@
 fi
