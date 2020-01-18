dc8be76924bcabee14eedace7a58cecc8032f054
root <admin@vpntech.net>
2020-01-17 22:20:26 -0500

    breka up client scripts

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ var handleInsert = function(ex, _cb) {
     //delete ex.line;
     if (ex.type == 'start') {
         delete ex.type;
-        ex.json_end = '{}';
+        //ex.json_end = '{}';
         ex.exec_name = path.posix.basename(ex.exec);
 
         try {
