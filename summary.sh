1814878fea62282214eb460357b198b3b497a5ea
root <admin@vpntech.net>
2020-01-18 00:10:05 -0500

    breka up client scripts

--- summary.sh
+++ summary.sh
@@ -7,12 +7,21 @@ doSQL(){
 
 }
 
+doSumColumn(){
+    echo "$1 $2 Sum:"
+    doSQL "SELECT $1, SUM($2) as total_time \
+    FROM execs \
+    GROUP BY $1 \
+    ORDER BY total_time DESC \
+    LIMIT 5"
+    echo
+}
 doFrequencyColumn(){
     echo "$1 Frequency:"
     doSQL "SELECT $1, COUNT(*) as frequency \
     FROM execs \
     GROUP BY $1 \
-    ORDER BY COUNT(*) DESC \
+    ORDER BY frequency DESC \
     LIMIT 5"
     echo
 }
@@ -23,6 +32,7 @@ doSummary(){
     doFrequencyColumn exec
     doFrequencyColumn user
     doFrequencyColumn exit_code
+    doSumColumn exec time
 }
 
 
