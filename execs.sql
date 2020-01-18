68900a5f54c9f1f7cee31f8eca3e4d8fa3d32662
root <admin@vpntech.net>
2020-01-17 19:22:50 -0500

    add add b64 encoded col

--- execs.sql
+++ execs.sql
@@ -3,18 +3,17 @@ DROP TABLE IF EXISTS `execs`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `execs` (
   `id` MEDIUMINT NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) default NULL,
-  `cmd` MEDIUMTEXT NOT NULL,
   `user` varchar(255) default NULL,
+  `cmd` MEDIUMTEXT NOT NULL,
   `pid` INT NOT NULL,
   `env` varchar(4096) default NULL,
-  `time` varchar(128) default NULL,
   `cwd` varchar(128) default  NULL,
   `exit_code` INT default NULL,
+  `line` MEDIUMTEXT NOT NULL,
+  `time` varchar(128) default NULL,
   `started_ts` timestamp NOT NULL default now(),
   `ended_ts` timestamp NULL,
   `line_b64` MEDIUMTEXT NOT NULL,
-  `line` MEDIUMTEXT NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
