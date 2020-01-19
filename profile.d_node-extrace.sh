684f96583c95edb894d47c02c521c32be980aade
root <admin@vpntech.net>
2020-01-19 09:14:37 -0500

    breka up client scripts

--- profile.d_node-extrace.sh
+++ profile.d_node-extrace.sh
@@ -1,10 +1,10 @@
-alias ne-summary=/opt//node-extrace/summary.sh
-alias ne-stop=/opt//node-extrace/stop.sh
-alias ne-start=/opt//node-extrace/start.sh
-alias ne-status=/opt//node-extrace/status.sh
-alias ne-install=/opt//node-extrace/install.sh
-alias ne-log=/opt//node-extrace/logs.sh
-alias ne-update=/opt//node-extrace/update.sh
-alias ne-delete=/opt//node-extrace/delete.sh
+alias ne-summary=/opt/node-extrace/summary.sh
+alias ne-stop=/opt/node-extrace/stop.sh
+alias ne-start=/opt/node-extrace/start.sh
+alias ne-status=/opt/node-extrace/status.sh
+alias ne-install=/opt/node-extrace/install.sh
+alias ne-log=/opt/node-extrace/logs.sh
+alias ne-update=/opt/node-extrace/update.sh
+alias ne-delete=/opt/node-extrace/delete.sh
 
 alias ne=ne-summary
